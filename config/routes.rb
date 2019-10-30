a53ddc8eef9bfe82cf7861a4e0a1760f7780faa2
Ben <benjisgarza@gmail.com>
2019-10-30 16:02:30 -0700

    Setup name attribute, add links, add tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  devise_scope :user do  
-    root to: "devise/sessions#new"
+  authenticated :user do 
+    root to: "posts#index"
   end
+  devise_scope :user do  
+    root to: "users/sessions#new", as: "login_root"
+  end
+
+  resources :posts
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,19 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   before :each do
     @user = User.new(name: 'tom', email: 'tom@gmail.com', password: '123456', password_confirmation: '123456')
+    @user.save
+    @post = @user.posts.build('Hello World!')
   end
 
-  it 'is valid user' do
-    expect(@user).to be_valid
+  it 'is valid post' do
+    expect(@post).to be_valid
   end
 
-  it 'is not a valid email' do
-    @user.email = 'tomgmail.com'
-    expect(@user).to_not be_valid
+  it 'is not a valid post' do
+    @post.user_id = nil
+    expect(@post).to_not be_valid
   end
 end
