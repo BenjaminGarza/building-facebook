0fc486b4deeb7754b92683d1b418501ddb329ec3
Juakata <ha.uzquiano@hotmail.com>
2019-11-08 16:05:33 -0600

    stickler issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      user.email = data['email'] if user.email.blank? if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
     end
   end
 
@@ -22,8 +21,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name
+      user.image = auth.info.image
     end
   end
 end
