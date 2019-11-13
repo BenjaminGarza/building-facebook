0ed4d6840a78d893cb8113361cddb7fc3619fdd2
Juakata <ha.uzquiano@hotmail.com>
2019-11-12 22:41:26 -0600

    environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,12 +6,6 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-  RAILS_ENV=production rake assets:precompile
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
